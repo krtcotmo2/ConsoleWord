e0849dcafe9263a6329d3d8a1409892739031e1f
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-04 21:20:13 -0500

    completed game play

--- Letter.js
+++ Letter.js
@@ -1,13 +1,6 @@
 function Letter(val){
      this.val = val;
      this.found=false;
-     
-     this.getFound = function(){
-          return this.found;
-     }
-     this.setFound = function(bool){
-          this.found = bool;
-     }
 }
 
 module.exports = Letter
--- Word.js
+++ Word.js
@@ -3,28 +3,45 @@ let Letter = require("./Letter.js");
 function Word(val){
      this.theWord = val;
      this.letters = [];
+     this.solved = false;
+     this.foundLetter = false;
 
      this.setWord = function(){
+          this.letters = [];
           for(let d=0; d<this.theWord.length; d++){
                let aLetter = new Letter(this.theWord.charAt(d));
-               if(aLetter.val == " " || aLetter.val.toLowerCase() == "r"|| aLetter.val.toLowerCase() == "e"|| aLetter.val.toLowerCase() == "t"|| aLetter.val.toLowerCase() == "p" ){
+               if(aLetter.val == " "){
                     aLetter.found = true;
                }
                this.letters.push(aLetter);
           }
      }
-     
 
      this.printWord = function(){
           let str = "";
-          for(let d=0; d< this.letters.length; d++){
-               if(this.letters[d].found){
-                    str = str + " " +this.letters[d].val;
+          this.letters.forEach(o => {
+               if(o.found){
+                    str = str + " " +o.val;
                }else{
                     str = str + " _";
                }
-          }
+          })
+          console.log("The password is:\n");
           console.log(str);
+          console.log("\n");
+     }
+
+     this.checkLetter = function(arg){
+          this.foundLetter = false;
+          this.letters.forEach( o => {  
+              if( o.val.toLowerCase() === arg.toLowerCase()){
+                    o.found = true;
+                    this.foundLetter = true;
+              }
+          });
+          this.solved = this.letters.filter( o => {
+               return o.found == false;
+          }).length == 0;
      }
 }
 
