31d273f02b9e12198021c7495aa4a18bc003fe35
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-05 18:33:46 -0500

    converted basic js file to make it into a single object

--- index.js
+++ index.js
@@ -1,5 +1,69 @@
 let Word = require("./Word");
 let inquirer = require("inquirer");
+
+let theGame = {
+     theWord:'',
+     numLeft:4,
+     wordList: ["emphatically", "covetous", "palpable", "intimation", "indignant", "derive", "impropriety", "homage", "destitute", "facetious", "proffer", "tacitly", "melancholy", "inexplicable", "incredulous", "waggish", "specter", "infernal", "deference", "indict", "susceptible", "forbearance", "incoherent", "repose", "resolution", "recumbent", "fluctuate", "intention", "conducive surmount", "retentive", "despondent", "laden", "expend", "condescension", "terrestrial", "tumult", "deft", "corroborate render", "avarice", "displace", "relentless", "comely", "subside"],
+     
+     askQuestion: function(){
+          askQuestion = this.askQuestion;
+          playAgain = this.playAgain;
+          //console.log(theWord);
+          theWord.printWord();
+          inquirer.prompt([{
+               name: "letterPicked",
+               message: "Please type a letter: "
+          }]).then(function(ans){
+               process.stdout.write('\033c');
+               theWord.checkLetter(ans.letterPicked);
+               if(!theWord.solved){
+                    if(!theWord.foundLetter && numLeft > 0){
+                         numLeft--;
+                         console.log(`${ans.letterPicked} in not in the password. You have ${numLeft} miss(es) remaining.`)
+                    }else if(!theWord.foundLetter && numLeft == 0){
+                         console.log(`${ans.letterPicked} in not in the password. You lost.\nThe password was ${theWord.theWord}`)
+                         playAgain();
+                         return;
+                    }
+                    askQuestion();
+               }else{
+                    console.log("You Won!")
+                    theWord.printWord();
+                    playAgain();
+               }
+          }).catch(function(err){
+               console.log(err);
+          });
+     },
+     startGame: function(){
+          process.stdout.write('\033c');
+          console.log("Vocabulary taken from Charles Dickens 'A Christmas Carol'")
+          let ind = Math.floor(Math.random()*this.wordList.length);
+          theWord = new Word(this.wordList[ind]);
+          numLeft = 4
+          theWord.setWord();
+          this.askQuestion();
+     },
+     playAgain: function(){
+          inquirer.prompt([{
+               name: "again",
+               type: "confirm",
+               message: "Would you like to play again? "
+          }]).then(function(ans){
+               if(ans.again){
+                    theGame.startGame();
+               }
+          }).catch(function(err){
+               console.log(err);
+          });
+     }
+}
+
+theGame.startGame();
+
+
+/*
 let theWord, numLeft;
 let wordList = ["emphatically", "covetous", "palpable", "intimation", "indignant", "derive", "impropriety", "homage", "destitute", "facetious", "proffer", "tacitly", "melancholy", "inexplicable", "incredulous", "waggish", "specter", "infernal", "deference", "indict", "susceptible", "forbearance", "incoherent", "repose", "resolution", "recumbent", "fluctuate", "intention", "conducive surmount", "retentive", "despondent", "laden", "expend", "condescension", "terrestrial", "tumult", "deft", "corroborate render", "avarice", "displace", "relentless", "comely", "subside"];
 
@@ -53,4 +117,5 @@ let playAgain = function(){
      });
 }
 
-setWord();
+//setWord();
+*/
